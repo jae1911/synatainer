b3c9cdfee800615d8b7fe499f8258568c1ea53f1
saces <saces@c-base.org>
2022-02-18 20:09:46 +0100

    stub

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM registry.gitlab.com/mb-saces/rust-synapse-compress-state:latest
+
+RUN apk add --no-cache bash curl jq
---
+++ scripts/synatainer.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# tag::doc[]
+# Automagic synapse maintenance script.
+# What it does:
+# 
+# end::doc[]
+
+set -ex
+
+exit 0
