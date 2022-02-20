3e66e50dc830bf7484c8505a8b218d5002de8ae4
saces <saces@c-base.org>
2022-02-20 21:15:22 +0100

    improve curl call

--- scripts/remote_cache_purge.sh
+++ scripts/remote_cache_purge.sh
@@ -11,6 +11,6 @@ CONFIG_FILE=/conf/synatainer.conf && test -f $CONFIG_FILE && source $CONFIG_FILE
 
 before_ts=$(expr $(date '+%s') - $(expr ${MEDIA_MAX_AGE:-90} \* 86400))
 
-curl --header "Authorization: Bearer $BEARER_TOKEN" \
+curl -sSL --header "Authorization: Bearer $BEARER_TOKEN" \
     -X POST -H "Content-Type: application/json" -d "{}" \
     "${SYNAPSE_HOST:-http://127.0.0.1:8008}/_synapse/admin/v1/purge_media_cache?before_ts=${before_ts}000"
