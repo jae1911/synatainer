2bb6c7659044d4feb4284678fc671277f48c0ad7
saces <saces@c-base.org>
2022-02-20 23:40:31 +0100

    make the vacuum script work..

--- scripts/vacuum-db.sh
+++ scripts/vacuum-db.sh
@@ -8,7 +8,9 @@ set -eux
 
 CONFIG_FILE=/conf/synatainer.conf && test -f $CONFIG_FILE && source $CONFIG_FILE
 
-psql_cmd="PGPASSWORD=$DB_PASSWORD psql -U $DB_USER -d $DB_NAME -h $DB_HOST"
+export PGPASSWORD=$DB_PASSWORD
+
+psql_cmd="psql -U $DB_USER -d $DB_NAME -h $DB_HOST"
 
 for table in $($psql_cmd --tuples-only -P pager=off -c "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';"); do
   $psql_cmd -c "VACUUM FULL VERBOSE $table;"
