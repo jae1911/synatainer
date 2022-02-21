c4a488d6cc352f791409bbc86efeb44dca50b488
saces <saces@c-base.org>
2022-02-21 01:00:34 +0100

    add background service :)

---
+++ scripts/background-service.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# tag::doc[]
+# Automagic synapse maintenance background service.
+# What it does (default):
+#    daily:
+#      purge all rooms without local members
+#      run the state autocompressor (small setting)
+#    weekly:
+#      delete old remote media
+#      delete old message history
+#    monthly:
+#      run the state autocompressor (big setting)
+#      vacuum the database
+# # end::doc[]
+
+set -eux
+
+CONFIG_FILE=/conf/synatainer.conf && test -f $CONFIG_FILE && source $CONFIG_FILE
+
+echo "not yet."
+
+#crond -f -l 8
+
+exit 0
