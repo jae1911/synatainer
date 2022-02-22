ccf35d9aec6a33d4fe3b877efbb713e285c662c8
saces <saces@c-base.org>
2022-02-22 01:03:07 +0100

    fix database password

--- scripts/vacuum-db.sh
+++ scripts/vacuum-db.sh
@@ -8,8 +8,6 @@ set -eu
 
 CONFIG_FILE=/conf/synatainer.conf && test -f $CONFIG_FILE && source $CONFIG_FILE
 
-export PGPASSWORD=$DB_PASSWORD
-
 psql_cmd="psql -U $DB_USER -d $DB_NAME -h $DB_HOST"
 
 for table in $($psql_cmd --tuples-only -P pager=off -c "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';"); do
