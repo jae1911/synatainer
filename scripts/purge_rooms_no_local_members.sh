53f95e454e175bc92d35496ac0174eaa7dfa9bc2
saces <saces@c-base.org>
2022-03-02 22:53:45 +0100

    make scripts a bit more verbose

--- scripts/purge_rooms_no_local_members.sh
+++ scripts/purge_rooms_no_local_members.sh
@@ -8,10 +8,13 @@ set -eu
 
 CONFIG_FILE=/conf/synatainer.conf && test -f $CONFIG_FILE && source $CONFIG_FILE
 
+echo "Purge rooms without local members."
+
 for room_id in $(curl -sSL --header "Authorization: Bearer $BEARER_TOKEN" \
     "${SYNAPSE_HOST:-http://127.0.0.1:8008}/_synapse/admin/v1/rooms?limit=1000&order_by=joined_local_members&dir=b" | jq -r '.rooms[] | select(.joined_local_members == 0) | .room_id'); do
-  echo "purge room $room_id ..."
+  echo "purge room $room_id"
   curl -sSL --header "Authorization: Bearer $BEARER_TOKEN" \
     -X DELETE -H "Content-Type: application/json" -d "{ \"purge\": true }" "${SYNAPSE_HOST:-http://127.0.0.1:8008}/_synapse/admin/v1/rooms/${room_id}"
-  echo "done."
 done
+
+echo "Done."
