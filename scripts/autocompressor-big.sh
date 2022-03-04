0355bb080c694acc25fa9bf583bf7c77c8cf5f45
saces <saces@c-base.org>
2022-03-04 09:28:55 +0100

    synapse_auto_compressor needs to be run in a writable dir

--- scripts/autocompressor-big.sh
+++ scripts/autocompressor-big.sh
@@ -8,4 +8,5 @@ set -eu
 
 CONFIG_FILE=/conf/synatainer.conf && test -f $CONFIG_FILE && source $CONFIG_FILE
 
+cd /tmp
 synapse_auto_compressor -p "user=$DB_USER password=$PGPASSWORD dbname=$DB_NAME host=$DB_HOST" -n ${STATE_AUTOCOMPRESSOR_CHUNKS_TO_COMPRESS_BIG:-300} -c ${STATE_AUTOCOMPRESSOR_CHUNK_SIZE_BIG:-1500}
